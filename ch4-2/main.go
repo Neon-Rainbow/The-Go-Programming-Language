63144daf1b23108113bd31f2ff60a38163680914
水告木南 <Haonan_Fang@Outlook.com>
2023-10-27 00:45:00 +0800

    add ch4-2 and ch4-3

---
+++ ch4-2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func reverseByPoint(arr *[6]int) {
+	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+}
+
+func equal(x, y []string) bool {
+    if len(x) != len(y) {
+        return false
+    }
+    for i := range x {
+        if x[i] != y[i] {
+            return false
+        }
+    }
+    return true
+}
+
+func main() {
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	reverse(a[:])
+	fmt.Println(a)
+	reverseByPoint(&a)
+	fmt.Println(a)
+}
